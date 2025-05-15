d446a545054d9c5c1ce9e1b5eabdccc194287ae3
katswa <160299048+katswa@users.noreply.github.com>
2025-05-15 12:56:57 -0700

    Delete js/api.js

--- js/api.js
+++
@@ -1,8 +0,0 @@
-/* This script uses an API to add interactivity to my site
-   It is inspired from JavaScript & jQuery book examples */
-
-$("#accordion").accordion({
-	active: 0, // Opens the first panel by default
-	collapsible: true, // Allows collapsing all panels
-	heightStyle: "content" // Adjust height based on content
-});
